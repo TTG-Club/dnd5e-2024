b2fea26c9c905188c3db74a3f1cf57ca5e56d14b
Peterko <peter.ko@mail.ru>
2026-08-01 11:45:01 +0300

    chore(release): v0.3.2

--- src/engine/dnd5eSystem.ts
+++ src/engine/dnd5eSystem.ts
@@ -210,7 +210,7 @@ export class Dnd5eVttSystem implements VttSystem {
 
   readonly name = 'Dungeons & Dragons 5th Edition';
 
-  readonly version = '0.3.1';
+  readonly version = '0.3.2';
 
   /**
    * Выполняет валидацию данных актера по правилам системы D&D 5e.
